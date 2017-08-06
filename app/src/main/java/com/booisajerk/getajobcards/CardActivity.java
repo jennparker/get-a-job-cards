80e01684dd8ca21e5c5a108721cb5eac08a231ff
Jenn Parker <jenniferparker070@gmail.com>
2017-08-05 19:38:37 -0700

    Fancy stuff: FAB animations, updated icons, background image for landing page

--- app/src/main/java/com/booisajerk/getajobcards/CardActivity.java
+++ app/src/main/java/com/booisajerk/getajobcards/CardActivity.java
@@ -1,6 +1,5 @@
 package com.booisajerk.getajobcards;
 
-import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +9,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,12 +19,11 @@ import android.widget.Toast;
  * Created by jenniferparker on 7/30/17.
  */
 
-public class CardActivity extends AppCompatActivity {
+public class CardActivity extends AppCompatActivity implements View.OnClickListener {
 
     private CardReaderDbHelper cardHelper;
 
     private boolean isShowAnswer;
-    TextView cardNumberText;
     TextView cardCategoryText;
     TextView cardQuestionText;
     TextView cardAnswerText;
@@ -31,15 +31,16 @@ public class CardActivity extends AppCompatActivity {
     TextView moreButton;
     String moreValue;
     int cardCount;
-    FloatingActionButton optionsFab;
-    FloatingActionButton editCardFab;
-    FloatingActionButton deleteCardFab;
+
+    private Boolean isFabOpen = false;
+    private FloatingActionButton optionsFab, editCardFab, deleteCardFab, addCardFab;
+    private Animation fab_open, fab_close, rotate_forward, rotate_backward;
 
     private static final String LOG_TAG = "Parker" + CardActivity.class.getSimpleName();
 
     private int cardNum = 3;
 
-    Card c;
+    Card card;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -53,17 +54,64 @@ public class CardActivity extends AppCompatActivity {
 
         Log.d(LOG_TAG, "Card count is: " + cardCount);
 
-        cardNumberText = (TextView) findViewById(R.id.questionNumberText);
         cardCategoryText = (TextView) findViewById(R.id.categoryText);
         cardQuestionText = (TextView) findViewById(R.id.cardQuestionText);
         cardAnswerText = (TextView) findViewById(R.id.cardAnswerText);
         advanceButton = (Button) findViewById(R.id.advanceButton);
         moreButton = (TextView) findViewById(R.id.moreButton);
-        optionsFab = (FloatingActionButton) findViewById(R.id.card__options_fab);
-        deleteCardFab = (FloatingActionButton) findViewById(R.id.card__delete_fab);
-        editCardFab = (FloatingActionButton) findViewById(R.id.card_edit_fab);
+        optionsFab = (FloatingActionButton) findViewById(R.id.fabOptions);
+        deleteCardFab = (FloatingActionButton) findViewById(R.id.fabDelete);
+        editCardFab = (FloatingActionButton) findViewById(R.id.fabEdit);
+        addCardFab = (FloatingActionButton) findViewById(R.id.fabAdd);
+        fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_backward);
+        optionsFab.setOnClickListener(this);
+        addCardFab.setOnClickListener(this);
+        editCardFab.setOnClickListener(this);
+        deleteCardFab.setOnClickListener(this);
 
         displayQuestion();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.fabOptions:
+
+                animateFAB();
+                break;
+
+            case R.id.fabAdd:
+
+                Intent addCardIntent = new Intent(this, AddCardActivity.class);
+                startActivity(addCardIntent);
+
+                Log.d(LOG_TAG, "Add Fab");
+                break;
+
+            case R.id.fabEdit:
+
+                Intent editCardintent = new Intent(CardActivity.this, EditCardActivity.class);
+
+                editCardintent.putExtra("current_question", card.getQuestion());
+                editCardintent.putExtra("current_answer", card.getAnswer());
+                editCardintent.putExtra("current_more", card.getMore());
+                editCardintent.putExtra("current_category", card.getCategory());
+                editCardintent.putExtra("current_id", card.getId());
+
+                startActivity(editCardintent);
+
+                Log.d(LOG_TAG, "Edit Fab");
+                break;
+
+            case R.id.fabDelete:
+                Toast.makeText(CardActivity.this, "Show delete card dialog", Toast.LENGTH_LONG).show();
+                Log.d(LOG_TAG, "Delete Fab");
+        }
     }
 
     @Override
@@ -81,7 +129,6 @@ public class CardActivity extends AppCompatActivity {
                         displayQuestion();
                     } else {
                         cardQuestionText.setText(R.string.no_more_cards_button_text);
-                        cardNumberText.setText(null);
                         cardCategoryText.setText(null);
                         cardAnswerText.setVisibility(View.INVISIBLE);
                         moreButton.setVisibility(View.INVISIBLE);
@@ -98,7 +145,7 @@ public class CardActivity extends AppCompatActivity {
                 AlertDialog.Builder dialog = new AlertDialog.Builder(CardActivity.this);
                 dialog.setMessage(moreValue);
 
-                dialog.setNegativeButton ("cancel", new DialogInterface.OnClickListener() {
+                dialog.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         dialog.dismiss();
                     }
@@ -109,52 +156,15 @@ public class CardActivity extends AppCompatActivity {
             }
         });
 
-        optionsFab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                optionsFab.setVisibility(View.INVISIBLE);
-
-                deleteCardFab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        //TODO add delete activity intent
-                        Toast.makeText(CardActivity.this, "Show delete card dialog", Toast.LENGTH_LONG).show();
-
-                    }
-                });
-                deleteCardFab.setVisibility(View.VISIBLE);
-
-                editCardFab.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-
-                        Intent editCardintent = new Intent(CardActivity.this, EditCardActivity.class);
-                        editCardintent.putExtra("current_question", c.getQuestion());
-                        editCardintent.putExtra("current_answer", c.getAnswer());
-                        editCardintent.putExtra("current_more", c.getMore());
-                        editCardintent.putExtra("current_category", c.getCategory());
-                        editCardintent.putExtra("current_id", c.getId());
-
-                        startActivity(editCardintent);
-
-                        Toast.makeText(CardActivity.this, "Show edit card dialog", Toast.LENGTH_LONG).show();
-
-                    }
-                });
-                editCardFab.setVisibility(View.VISIBLE);
-            }
-        });
     }
 
     private void displayQuestion() {
-        c = cardHelper.getCard(cardNum);
-        cardNumberText.setText(Integer.toString(c.getId()));
-        cardQuestionText.setText(c.getQuestion());
+        card = cardHelper.getCard(cardNum);
+        cardQuestionText.setText(card.getQuestion());
         cardAnswerText.setVisibility(View.INVISIBLE);
-        cardAnswerText.setText(c.getAnswer());
-        moreValue = c.getMore();
-        cardCategoryText.setText(c.getCategory());
+        cardAnswerText.setText(card.getAnswer());
+        moreValue = card.getMore();
+        cardCategoryText.setText(card.getCategory());
         advanceButton.setText(R.string.show_answer_text);
         isShowAnswer = true;
         cardQuestionText.setTextColor(getResources().getColor(R.color.textMain));
@@ -168,6 +178,33 @@ public class CardActivity extends AppCompatActivity {
         isShowAnswer = false;
         cardNum++;
         Log.d(LOG_TAG, "Display answer called");
+    }
 
+    public void animateFAB() {
+
+        if (isFabOpen) {
+
+            optionsFab.startAnimation(rotate_backward);
+            addCardFab.startAnimation(fab_close);
+            editCardFab.startAnimation(fab_close);
+            deleteCardFab.startAnimation(fab_close);
+            addCardFab.setClickable(false);
+            editCardFab.setClickable(false);
+            deleteCardFab.setClickable(false);
+            isFabOpen = false;
+            Log.d(LOG_TAG, "closing fabs");
+
+        } else {
+
+            optionsFab.startAnimation(rotate_forward);
+            addCardFab.startAnimation(fab_open);
+            editCardFab.startAnimation(fab_open);
+            deleteCardFab.startAnimation(fab_open);
+            addCardFab.setClickable(true);
+            editCardFab.setClickable(true);
+            deleteCardFab.setClickable(true);
+            isFabOpen = true;
+            Log.d(LOG_TAG, "opening fabs");
+        }
     }
 }
